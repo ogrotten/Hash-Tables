2b731171a850aba47446d8d26ebe31222537c8e8
chip <chiposaur@gmail.com>
2020-04-06 16:34:10 -0500

    yeah, well. . .

--- src/hashtable.py
+++ src/hashtable.py
@@ -64,7 +64,7 @@ class HashTable:
 			print("location used")
 			return "location used"
 		
-		self.storage[getloc] = {key, value}
+		self.storage[getloc] = {key: value}
 		print(self.storage)
 
 		# return 
@@ -128,8 +128,11 @@ class HashTable:
 			if item == None:
 				continue
 
-			oldkey = item[0]
-			oldval = item[1]
+			print(131,item)
+			oldkey = item.items()[0]
+			print(131,oldkey)
+			oldval = item[oldkey]
+			print(131,item)
 			
 			self.insert(oldkey, oldval)
 
