3a12492315addc4c2d795393c6d53fbca8c56fb1
chip <chiposaur@gmail.com>
2020-04-06 14:41:59 -0500

    insert completed.

--- src/hashtable.py
+++ src/hashtable.py
@@ -2,119 +2,127 @@
 # Linked List hash table key/value pair
 # '''
 class LinkedPair:
-    def __init__(self, key, value):
-        self.key = key
-        self.value = value
-        self.next = None
+	def __init__(self, key, value):
+		self.key = key
+		self.value = value
+		self.next = None
+
 
 class HashTable:
-    '''
-    A hash table that with `capacity` buckets
-    that accepts string keys
-    '''
-    def __init__(self, capacity):
-        self.capacity = capacity  # Number of buckets in the hash table
-        self.storage = [None] * capacity
+	"""
+	A hash table that with `capacity` buckets
+	that accepts string keys
+	"""
 
+	
+	def __init__(self, capacity):
+		self.capacity = capacity  # Number of buckets in the hash table
+		self.storage = [None] * capacity
 
-    def _hash(self, key):
-        '''
-        Hash an arbitrary key and return an integer.
+	def _hash(self, key):
+		"""
+		Hash an arbitrary key and return an integer.
 
-        You may replace the Python hash with DJB2 as a stretch goal.
-        '''
-        return hash(key)
+		You may replace the Python hash with DJB2 as a stretch goal.
+		"""
+		return hash(key)
 
+	def _hash_djb2(self, key):
+		"""
+		Hash an arbitrary key using DJB2 hash
 
-    def _hash_djb2(self, key):
-        '''
-        Hash an arbitrary key using DJB2 hash
+		OPTIONAL STRETCH: Research and implement DJB2
+		"""
+		pass
 
-        OPTIONAL STRETCH: Research and implement DJB2
-        '''
-        pass
+	def _hash_mod(self, key):
+		"""
+		Take an arbitrary key and return a valid integer index
+		within the storage capacity of the hash table.
+		"""
+		return self._hash(key) % self.capacity
 
+	def insert(self, key, value):
+		#region
+		"""
+		Store the value with the given key.
 
-    def _hash_mod(self, key):
-        '''
-        Take an arbitrary key and return a valid integer index
-        within the storage capacity of the hash table.
-        '''
-        return self._hash(key) % self.capacity
+		# Part 1: Hash collisions should be handled with an error warning. (Think about and
+		# investigate the impact this will have on the tests)
 
+		# Part 2: Change this so that hash collisions are handled with Linked List Chaining.
 
-    def insert(self, key, value):
-        '''
-        Store the value with the given key.
+		Fill this in.
+		"""
+		#endregion
 
-        # Part 1: Hash collisions should be handled with an error warning. (Think about and
-        # investigate the impact this will have on the tests)
+		gethash = self._hash(key)
+		getloc = self._hash_mod(gethash)
 
-        # Part 2: Change this so that hash collisions are handled with Linked List Chaining.
+		# print(62,key, gethash,getloc)
+		if self.storage[getloc] != None:
+			print("location used")
+			return "location used"
+		
+		self.storage[getloc] = {key, value}
+		print(self.storage)
 
-        Fill this in.
-        '''
-        pass
+		# return 
 
+	def remove(self, key):
+		"""
+		Remove the value stored with the given key.
 
+		Print a warning if the key is not found.
 
-    def remove(self, key):
-        '''
-        Remove the value stored with the given key.
+		Fill this in.
+		"""
+		pass
 
-        Print a warning if the key is not found.
+	def retrieve(self, key):
+		"""
+		Retrieve the value stored with the given key.
 
-        Fill this in.
-        '''
-        pass
+		Returns None if the key is not found.
 
+		Fill this in.
+		"""
+		pass
 
-    def retrieve(self, key):
-        '''
-        Retrieve the value stored with the given key.
+	def resize(self):
+		"""
+		Doubles the capacity of the hash table and
+		rehash all key/value pairs.
 
-        Returns None if the key is not found.
-
-        Fill this in.
-        '''
-        pass
-
-
-    def resize(self):
-        '''
-        Doubles the capacity of the hash table and
-        rehash all key/value pairs.
-
-        Fill this in.
-        '''
-        pass
-
+		Fill this in.
+		"""
+		pass
 
 
 if __name__ == "__main__":
-    ht = HashTable(2)
+	ht = HashTable(2)
 
-    ht.insert("line_1", "Tiny hash table")
-    ht.insert("line_2", "Filled beyond capacity")
-    ht.insert("line_3", "Linked list saves the day!")
+	ht.insert("line_1", "Tiny hash table")
+	ht.insert("line_2", "Filled beyond capacity")
+	ht.insert("line_3", "Linked list saves the day!")
 
-    print("")
+	print("")
 
-    # Test storing beyond capacity
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+	# Test storing beyond capacity
+	print(ht.retrieve("line_1"))
+	print(ht.retrieve("line_2"))
+	print(ht.retrieve("line_3"))
 
-    # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
+	# Test resizing
+	old_capacity = len(ht.storage)
+	ht.resize()
+	new_capacity = len(ht.storage)
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+	print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+	# Test if data intact after resizing
+	print(ht.retrieve("line_1"))
+	print(ht.retrieve("line_2"))
+	print(ht.retrieve("line_3"))
 
-    print("")
+	print("")
